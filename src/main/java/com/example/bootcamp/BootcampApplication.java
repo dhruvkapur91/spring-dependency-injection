163e344e450a03537578e17ffdb14f148c6b0792
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-10-26 09:35:24 +0530

    Using a void bean / provider method

--- src/main/java/com/example/bootcamp/BootcampApplication.java
+++ src/main/java/com/example/bootcamp/BootcampApplication.java
@@ -1,43 +1,18 @@
 package com.example.bootcamp;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @SpringBootApplication
 public class BootcampApplication {
 
     @Bean
-    Bar bar(Foo foo, UuidService uuidService) {
-        return new Bar(foo, uuidService.buildUuid());
+    void dummyBean() {
+        System.out.println("This got executed");
     }
 
     public static void main(String[] args) {
         SpringApplication.run(BootcampApplication.class, args);
-    }
-}
-
-@Component("uuid")
-class UuidService {
-    public String buildUuid() {
-        return UUID.randomUUID().toString();
-    }
-}
-
-class Bar {
-    public Bar(Foo foo, String uuid) {
-        System.out.println(uuid);
-        System.out.println("created bar");
-    }
-}
-
-@Component
-class Foo {
-    public Foo() {
-        System.out.println("created Foo");
     }
 }
