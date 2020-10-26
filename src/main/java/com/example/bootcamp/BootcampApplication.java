1e096c1516993d32aee67fc894c10b4d5a1d7b8f
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-10-26 09:43:40 +0530

    Inject a dependecy in void type provider method

--- src/main/java/com/example/bootcamp/BootcampApplication.java
+++ src/main/java/com/example/bootcamp/BootcampApplication.java
@@ -1,18 +1,27 @@
 package com.example.bootcamp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BootcampApplication {
 
     @Bean
-    void dummyBean() {
+    void dummyBean(Foo foo) {
         System.out.println("This got executed");
     }
 
     public static void main(String[] args) {
         SpringApplication.run(BootcampApplication.class, args);
+    }
+}
+
+@Component
+class Foo {
+    public Foo() {
+        System.out.println("Created foo");
     }
 }
