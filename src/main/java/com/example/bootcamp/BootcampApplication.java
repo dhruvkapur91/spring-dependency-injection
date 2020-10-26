8bf0857ec9238dadd5e830263cec654fcab448bb
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-10-26 12:19:08 +0530

    environment bean means something special

--- src/main/java/com/example/bootcamp/BootcampApplication.java
+++ src/main/java/com/example/bootcamp/BootcampApplication.java
@@ -4,42 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BootcampApplication {
 
+    @Bean
+    String environment(@Value("${environment}") String environment) {
+        System.out.println(environment);
+        return environment;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BootcampApplication.class, args);
-    }
-}
-
-interface KeyValueWriter {
-    public void write(String key, String value);
-}
-
-@Component("test")
-class TestWriter implements KeyValueWriter {
-
-    public TestWriter() {
-        System.out.println("Initialized test writer");
-    }
-
-    @Override
-    public void write(String key, String value) {
-        System.out.println("Using the test implementation");
-    }
-}
-
-@Component("prod")
-class ProdWriter implements KeyValueWriter {
-
-    public ProdWriter() {
-        System.out.println("Initialized prod writer");
-    }
-
-    @Override
-    public void write(String key, String value) {
-        System.out.println("Using the production implementation");
     }
 }
