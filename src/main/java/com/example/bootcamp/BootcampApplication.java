62f9c899c09da6920d617e259cc15c4c4c80a33b
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-10-26 12:28:10 +0530

    Putting the dependency directly in the method call

--- src/main/java/com/example/bootcamp/BootcampApplication.java
+++ src/main/java/com/example/bootcamp/BootcampApplication.java
@@ -3,18 +3,29 @@ package com.example.bootcamp;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class BootcampApplication {
 
-    @Bean
-    String environment(@Value("${environment}") String environment) {
-        System.out.println(environment);
-        return environment;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(BootcampApplication.class, args);
+    }
+}
+
+@Component("uuid")
+class UuidService {
+    public String getUuid() {
+        return UUID.randomUUID().toString();
+    }
+}
+
+@Component
+class Bar {
+    public Bar(@Value("#{uuid.getUuid()}") String uuid) {
+        System.out.println("Getting UUID directly");
+        System.out.println(uuid);
     }
 }
